f53bcac7a420a620a1b1d1f5cb24c903dac192e3
Abhinandan Shrestha <shtabhi@gmail.com>
2021-06-07 16:55:37 +0545

    first day

---
+++ todo_list/base/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Task
+# Create your views here.
+
+class TaskList(ListView): #by default looks for templates/base/task_list.html
+	model = Task 
+	context_object_name = 'tasks'
+
+class TaskDetail(DetailView): #by default looks for templates/base/task_detail.html
+	model = Task
+	context_object_name = 'task'
+	template_name ='base/task.html'
+
+class TaskCreate(CreateView): #by default looks for templates/base/task_form.html
+	model = Task
+	fields = '__all__' #fires all the modelForms
+	success_url = reverse_lazy('tasks') #after success re-directs to url 'tasks'
