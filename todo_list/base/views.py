cc1134ec6131092b38bde31578ad76c9a842c161
Abhinandan Shrestha <shtabhi@gmail.com>
2021-06-10 00:37:55 +0545

    login & restricted page

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -4,29 +4,42 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 # Create your views here.
 
-class TaskList(ListView): #by default looks for templates/base/task_list.html
+class CustomLoginView(LoginView):
+	template_name = 'base/login.html'
+	fields = '__all__'
+	redirect_authenticated_user = True
+
+	def get_success_url(self):
+		return reverse_lazy('tasks')
+
+class TaskList(LoginRequiredMixin, ListView): #by default looks for templates/base/task_list.html
 	model = Task 
 	context_object_name = 'tasks'
 
-class TaskDetail(DetailView): #by default looks for templates/base/task_detail.html
+class TaskDetail(LoginRequiredMixin, DetailView): #by default looks for templates/base/task_detail.html
 	model = Task
 	context_object_name = 'task'
 	template_name ='base/task.html'
 
-class TaskCreate(CreateView): #by default looks for templates/base/task_form.html
+class TaskCreate(LoginRequiredMixin, CreateView): #by default looks for templates/base/task_form.html
 	model = Task
 	fields = '__all__' #fires all the modelForms
 	success_url = reverse_lazy('tasks') #after success re-directs to url 'tasks'
 
-class TaskUpdate(UpdateView): #by default looks for templates/base/task_form.html
+class TaskUpdate(LoginRequiredMixin, UpdateView): #by default looks for templates/base/task_form.html
 	model = Task
 	fields = '__all__' #fires all the modelForms
 	success_url = reverse_lazy('tasks') #after success re-directs to url 'tasks'
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
 	model = Task
 	context_object_name = 'task'
 	success_url = reverse_lazy('tasks')
